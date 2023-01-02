e1fb4e382dac25be2e0abb6bb21b6a0a1cb41ec7
sundarPipelines <111037730+DevopsPipelines03@users.noreply.github.com>
2023-01-02 08:40:19 +0530

    Delete DeployOptions directory

--- DeployOptions/Angular/src/assets/env.template.js
+++
@@ -1,7 +0,0 @@
-(function(window) {
-  window.env = window.env || {};
-
-  // Environment variables
-  window["env"]["API_URL"] = "${API_URL}";
-  window["env"]["APPLICATIONINSIGHTS_INSTRUMENTATION_KEY"] = "${APPLICATIONINSIGHTS_INSTRUMENTATION_KEY}";
-})(this);
--- DeployOptions/Angular/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: (<any>window).env.API_URL,
-  instrumentationKey: (<any>window).env.APPLICATIONINSIGHTS_INSTRUMENTATION_KEY,
-};
